ee8dc575d7b370ffccd9e4d372ff522c945b5a0b
Glitch (img-s-a) <none>
2018-04-08 02:01:08 +0000

    [final 1.0.0]

--- server.js
+++ server.js
@@ -1,32 +1,75 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const moment = require('moment');
+const request = require('request');
+const rp = require('request-promise');
+const cors = require('cors');
+const mongodb = require('mongodb'); 
 
-app.use(express.static('public'))
+const app = express();
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
+app.use(cors());
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
+let MongoClient = mongodb.MongoClient;
 
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
+app.use(express.static(__dirname + '/public'));
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
-})
+app.get('/',(req,res)=>{
+
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/latest',(req,res)=>{
+      MongoClient.connect(process.env.DB_URL,(err,db)=>{
+            if(err) throw err;
+       let dbo = db.db('url-short-rdp');
+        dbo.collection('search-history').find({}).project({_id: 0}).toArray((err,result)=>{
+                      
+             res.json(result);
+            db.close();
+        
+        });        
+      });              
+});
+
+app.get('/api/search?(*)',(req,res)=>{
+       let toOutput = {};
+       let queryTerm = req.query.q;
+       let offset = req.query.offset;
+       //console.log(offset);
+       let urlToQuery = 'https://www.googleapis.com/customsearch/v1?key=' + process.env.G_APIKEY +  '&cx=' + process.env.G_ID + '&q=' + queryTerm + '&searchType=image' + '&startPage=' + offset + '&fields=items(link,snippet,image/thumbnailLink,image/contextLink)';
+
+      //here is an http request
+       rp(urlToQuery).then(function(data){
+          //console.log(data);
+            toOutput = data;
+          let objToHistory = {term: queryTerm , when:  moment().format('DD-MMMM-YYYY  HH:mm:ss a')};
+          
+        MongoClient.connect(process.env.DB_URL,(err,db)=>{
+                 if (err) throw err;
+          
+          let dbo = db.db('url-short-rdp');
+          dbo.createCollection('search-history',(err)=>{
+            if (err) throw err;
+            
+          });
+          
+          dbo.collection('search-history').insertOne(objToHistory,(err,res)=>{
+             if(err) throw err;
+            db.close();
+          });
+          
+        
+        }); 
+         
+       }).then(()=>{
+         
+          res.send("<pre>"+ toOutput + "</pre>");
+       
+       });
+     
+  
+});
+
+app.listen(process.env.PORT,(listener)=> console.log( process.env.PORT));
